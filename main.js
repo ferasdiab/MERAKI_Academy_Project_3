56f88761fe904e7f3ae1545a211be9b0dbe56bd6
ferasdiab <dfiras96@gmail.com>
2021-05-15 15:52:19 +0300

    getAllArticles

--- main.js
+++ main.js
@@ -5,8 +5,34 @@ const port = 5000;
 app.use(express.json());
 
 ////
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
 
-
+app.get("/articles",(req,res)=>{
+    res.status(200)
+    res.json(articles)
+})
 
 
 
