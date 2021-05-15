36973ce09bbe3f552d6308599617699811f388f0
ferasdiab <dfiras96@gmail.com>
2021-05-15 17:24:52 +0300

    createNewArticle

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { uuid } = require('uuidv4');
+
+
 
 const app = express();
 const port = 5000;
@@ -6,81 +9,84 @@ app.use(express.json());
 
 ////
 const articles = [
-    {
+  {
     id: 1,
-    title: 'How I learn coding?',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    {
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
     id: 2,
-    title: 'Coding Best Practices',
-    description:
-    'Lorem, ipsum dolor sit, Quam, mollitia.',
-    author: 'Besslan',
-    },
-    {
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
     id: 3,
-    title: 'Debugging',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    ];
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
 
- const getAllArticles=(req,res)=>{
-        res.status(200)
-        res.json(articles)
-    }
+const getAllArticles = (req, res) => {
+  res.status(200);
+  res.json(articles);
+};
 
-app.get("/articles",getAllArticles)
+app.get("/articles", getAllArticles);
 
 //////////////////////////////////////////////////
 
-const getArticlesByAuthor =  (req, res) => {
-    // query parameters way: "/articles/search_1?author=Jouza"
-    const author = req.query.author
+const getArticlesByAuthor = (req, res) => {
+  // query parameters way: "/articles/search_1?author=Jouza"
+  const author = req.query.author;
 
-    const arr = articles.filter((element,index)=>{
-
-        return author === element.author
-    })
-    res.status(200)
-    res.json(arr)
-
-      }
+  const arr = articles.filter((element, index) => {
+    return author === element.author;
+  });
+  res.status(200);
+  res.json(arr);
+};
 app.get("/articles/search_1", getArticlesByAuthor);
 
 ////////////////////////////
 
-const getAnArticleById = (req,res)=>{
-    const id = req.query.id
-    const found = articles.find((element,index) => {
-        return element.id === parseInt(id);
-      });
-    if (found) {
-    console.log(found)
-    res.status(200)
-    res.json(found)
-    }else{
-        res.status(404)
-    res.json("not found")
-    }
-
-}
+const getAnArticleById = (req, res) => {
+  const id = req.query.id;
+  const found = articles.find((element, index) => {
+    return element.id === parseInt(id);
+  });
+  if (found) {
+    console.log(found);
+    res.status(200);
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("not found");
+  }
+};
 
 app.get("/articles/search_2", getAnArticleById);
 
+///////////////////////////////////////////////////////
+const createNewArticle =  (req,res)=>{
+    const obj = {
+        title: req.body.title,
+        description:req.body.description,
+        author: req.body.author,
+        id : uuid(),
+    }
+    articles.push(obj)
 
-
-
-
-
-
-
+    res.status(201)
+    res.json(obj)
+}
+app.post("/articles",createNewArticle)
 ///////////////////////////////////////////////////////////////////
 
+
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
